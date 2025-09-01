b12455539bb22387b1b25a59077dce212f803d76
Daniel Komissarov <145659672+Argonkom@users.noreply.github.com>
2025-09-01 18:28:52 +0300

    main.py

---
+++ task_4_2/main.py
@@ -0,0 +1,8 @@
+#main.py
+def findNum(str):
+    for i in range(len(str)):
+        count = 0
+        for j in range(len(str)):
+            if str[i] == str[j]:
+                count += 1
+        print(f"{str[i]}, occured {count} time s")
